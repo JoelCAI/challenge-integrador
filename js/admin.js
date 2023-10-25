1f3d82f6c9da0918a9a9794c43e65876ba08a73c
JoelCAI <102713088+JoelCAI@users.noreply.github.com>
2023-10-25 12:53:12 -0300

    [FEAT]- Archivo Index reubicado

---
+++ js/admin.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const hero = {
+//     name: 'Bruce',
+//     lastName: 'Banner',
+//     alias: 'Hulk',
+//     saludar : function() {
+//         console.log(this.comida)
+//     },
+//     comida: 'alimento'
+// }
+
+// for (const key in hero) {
+//     console.log(key + " " + hero[key])
+// }
+
+// hero.saludar()
+
+
+
+
+
+
+
+
+
